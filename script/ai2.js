b5471af494b8c82a8e8de2c957c5ec43579cc5ac
ZephyrusFrost <141444827+ZephyrusFrost@users.noreply.github.com>
2024-03-31 01:49:04 +0800

    Create ai2.js

---
+++ script/ai2.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+module.exports.config = {
+  name: "ai",
+  version: "69",
+  role: 0,
+  credits: "OtinXSandip", // converted by kira
+  description: "ask AI",
+  usages: "ask <question>",
+  hasPrefix: false,
+  commandCategory: "ai",
+  cooldowns: 0
+};
+  
+module.exports.run = async function ({ api, event, args, message }) {
+  try {
+    const prompt = event.body.trim();
+    if (!prompt) {
+      await api.sendMessage({ body: "Hey I am Ai, ask me questions dear 🤖" }, event.threadID);
+      return;
+    }
+    api.setMessageReaction("🔎", event.messageID, (err) => {}, true);
+    const response = await axios.get(`https://sandipapi.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+    api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+    const answer = response.data.answer;
+
+    await api.sendMessage({
+      body: `𝗕𝗢𝗧 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 | 🟢
+━━━━━━━━━━━━━━━━━━        
+${answer}
+━━━━━━━━━━━━━━━━━━\n\n- 𝚃𝚑𝚒𝚜 𝚋𝚘𝚝 𝚞𝚗𝚍𝚎𝚛 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝚋𝚢 Kyle Bait-it\n• 𝗙𝗕𝗟𝗶𝗡𝗞: >>https://www.facebook.com/kyleyukaro<<`,
+    }, event.threadID);
+
+  } catch (error) {
+    console.error("🔴 An error occurred while processing your request.\nPlease contact churchill abing for an error", error.message);
+    api.setMessageReaction("🔴", event.messageID, (err) => {}, true);
+  }
+};
